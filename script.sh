a544194152fe7a1d302fa5655fdc1b0159aabcdd
finite-codez <finite.plays.inifinite@gmail.com>
2025-05-19 13:28:28 +0530

    hurray! script.sh is now ready for me
    i write commit messages like retards don't mind.

--- script.sh
+++ script.sh
@@ -1,25 +1,25 @@
 rm -rf .repo/local_manifests/
 
 #repo init
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 15.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/finite-codez/fetch.git -b lineage-22.2 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone https://github.com/blazey66/local_manifest_clo.git -b a15 .repo/local_manifests
-echo "============================"
+git clone https://github.com/AsTechpro20/local_manifests_blossom.git -b lineage-22.1 .repo/local_manifests
+echo "============================
 echo "Local manifest clone success"
 echo "============================"
 
 #Sync
 /opt/crave/resync.sh
-echo "============="
-echo "Sync success"
-echo "============="
+echo "================="
+echo " RESYNC STARTED! "
+echo "================="
 
 # Export
-export BUILD_USERNAME=Blazey66
+export BUILD_USERNAME=finitecode
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
@@ -28,4 +28,5 @@ echo "======= Export Done ======"
 echo "====== Envsetup Done ======="
 
 #build
-brunch Mi439_4_19 user
+echo "====== Starting build, you may pray ======="
+brunch blossom user
